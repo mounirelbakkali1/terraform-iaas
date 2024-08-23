38e5735919509cd6b16b9d5b8144595b896aa44c
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-08-23 22:41:40 +0100

    provision aws ec2 instence

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "ni" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-c998b6b2" 
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id = aws_network_interface.ni.id
+    device_index         = 0
+  }
+  tags = {
+    Name = "ExampleAppServerInstanceByTerraform"
+  }
+}
