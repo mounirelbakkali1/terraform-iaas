a7114dc861773547a97306f61f66cc1ded1f6f59
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-08-24 16:23:10 +0100

    EC2: security group, key pair, user data..

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -19,11 +8,35 @@ resource "aws_default_vpc" "default" {
   }
 }
 
+resource "aws_security_group" "web_servers_security_group" {
+  name = "allow HTTP requests"
+  ingress {
+    description = "Allow http traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.instance_name
+  }
+
+}
+
 resource "aws_instance" "web_server" {
   ami           = "ami-00c39f71452c08778" # AMI IDs are region-specific
   instance_type = "t2.micro"
-  count         = 2
-  depends_on    = [aws_default_vpc.default]
+  #count         = 2
+  depends_on      = [aws_default_vpc.default]
+  user_data       = file("init-script.sh")
+  key_name        = "mykeypair"
+  security_groups = [aws_security_group.web_servers_security_group.name]
 
   tags = {
     Name = var.instance_name
--- output.tf
+++ output.tf
@@ -1,9 +1,16 @@
-output "instance_ids" {
+output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = [for instance in aws_instance.web_server : instance.public_ip]
+  #value       = [for instance in aws_instance.web_server : instance.public_ip]
+  value = aws_instance.web_server.id
 }
 
-output "instance_public_ips" {
+output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = [for instance in aws_instance.web_server : instance.public_ip]
+  # value       = [for instance in aws_instance.web_server : instance.public_ip]
+  value = aws_instance.web_server.public_ip
+}
+
+output "application-url" {
+  #value = [for instance in aws_instance.web_server : "${aws_instance.web_server.public_ip}/index.php"]
+  value = "${aws_instance.web_server.public_ip}/index.php"
 }
